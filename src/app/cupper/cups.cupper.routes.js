3ddc0516da73903117e7d6c8879d9c0e4b90f8a0
ronaldboehm <github@f0ffff.net>
2015-11-03 14:28:45 +0100

    routing

--- src/app/cupper/cups.cupper.routes.js
+++ src/app/cupper/cups.cupper.routes.js
@@ -5,13 +5,17 @@
     .module('cups.cupper')
     .config(routes);
 
-  routes.$inject = ['$stateProvider'];
-  function routes($stateProvider) {
+  routes.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function routes($stateProvider, $urlRouterProvider) {
     $stateProvider
     .state('welcome', {
       url: '/welcome',
       templateUrl: './cupper/welcomeCupper.html',
       controller: 'cupperController as cupper'
     });
+    
+    $urlRouterProvider
+      .when('/welcome-a-new-cupper', '/welcome')
+      .otherwise('/');
   }
 }());
