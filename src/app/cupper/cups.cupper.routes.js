c84f702802f01bfd64d4d101005005ea1593f7de
ronaldboehm <github@f0ffff.net>
2015-11-03 10:40:32 +0100

    angular module and routes

---
+++ src/app/cupper/cups.cupper.routes.js
@@ -0,0 +1,16 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('cups.cupper')
+    .config(routes);
+
+  routes.$inject = ['$stateProvider'];
+  function routes($stateProvider) {
+    $stateProvider
+    .state('welcome', {
+      url: '/welcome',
+      templateUrl: 'cupper/welcomeCupper.html'
+    });
+  }
+}());
