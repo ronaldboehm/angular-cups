b87339c212d9fb272bf532c27e66b0b2ce7d546e
ronaldboehm <github@f0ffff.net>
2015-11-04 10:42:25 +0100

    some tests

--- test/cups.core.filter.spec.js
+++ test/cups.core.filter.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('When passing 10 to the filter', function() {
+describe('Testing the toCollection', function() {
 
   var $filter;
 
@@ -10,10 +10,25 @@ describe('When passing 10 to the filter', function() {
   }));
 
 
-  it('should create an array of length 10', function() {
-
-    var result = $filter('toCollection')(10);
-
-    expect(result.length).toBe(10);
+  describe('When passing 10 to the filter', function() {
+    it('should create an array of length 10', function() {
+      var result = $filter('toCollection')(10);
+      expect(result.length).toBe(10);
+    });
   });
+
+  describe('When passing 0 to the filter', function() {
+    it('should return not an array but the value passed to it', function() {
+      var result = $filter('toCollection')(0);
+      expect(result).toBe(0);
+    });
+  });
+
+  describe('When passing characters to the filter', function() {
+    it('should return not an array but the value passed to it', function() {
+      var result = $filter('toCollection')('aasf');
+      expect(result).toBe('aasf');
+    });
+  });
+
 });
